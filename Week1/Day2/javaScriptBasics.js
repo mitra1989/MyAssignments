301f288d648071ec1757dbd21aafce90bfa0bf1a
Shirsendu Mitra <m_shirsendu@yahoo.com>
2026-09-02 19:19:03 +0530

    restructuring folders

---
+++ Week1/Day2/javaScriptBasics.js
@@ -0,0 +1,19 @@
+var firstName = "John";
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "ABC Corp";
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 7845394369;
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
