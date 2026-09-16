e04174d0cb7f6e76502867886a5a2e2155e6f750
Shirsendu Mitra <m_shirsendu@yahoo.com>
2026-09-16 20:25:47 +0530

    Week2/Day4/editLead HomeWork

--- tests/Week2/Day4/editLead.spec.ts
+++ tests/Week2/Day4/editLead.spec.ts
@@ -13,11 +13,34 @@ test('edit lead with css selector', async ({ page }, testInfo) => {
   //Click login button
   await page.locator('[type="submit"]').click()
 
-  //click on CRM/SFA link
+  //Click on CRM/SFA link
   await page.locator('[for="crmsfa"] > a').click()
 
-  //click on Leads link
+  //Click on Leads link
   await page.locator('[href="/crmsfa/control/leadsMain"]').click()
 
-  
+  //Click on Create Lead link
+  await page.locator('[href="/crmsfa/control/createLeadForm"]').click()
+
+  //Enter company name
+  await page.locator('table [name="companyName"]').fill('TestLeaf')
+
+  //Enter first name
+  await page.locator('table [name="firstName"]').fill('Shirsendu')
+
+  //Enter last name
+  await page.locator('table [name="lastName"]').fill('Mitra')
+
+  //Click on Create Lead button
+  await page.locator('table [value="Create Lead"]').click()
+
+  //Click on Edit button
+  await page.locator('.frameSectionExtra .subMenuButton').nth(2).click()
+
+  //change the company name
+  await page.locator('table [name="companyName"]').fill('')
+  await page.locator('table [name="companyName"]').fill('TestLeaf Updated')
+
+  //Click on Update button
+  await page.locator('table [value="Update"]').click()
 });
